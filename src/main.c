43f43124370b87474d2f65167c7a31dfc463f1da
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-21 21:20:32 +0100

    print lines

--- src/main.c
+++ src/main.c
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 17:11:45 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/12/21 21:18:13 by poss             ###   ########.fr       */
+/*   Updated: 2023/12/21 21:20:23 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -36,5 +36,21 @@ int main(void)
 		}
 		i++;
 	}
+	printf("first line\n");
 	printf("%s\n", buffer);
+
+	bzero(buffer, BUFFER_SIZE);
+	i = 0;
+	while (read(fd, buffer + i, 1) > 0)
+	{
+		if (buffer[i] == '\n')
+		{
+			buffer[i] = 0;
+			break;
+		}
+		i++;
+	}
+	printf("second line\n");
+	printf("%s\n", buffer);
+
 }
