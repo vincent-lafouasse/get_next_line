a205d61db1b3c8ca48a2efecb75bbaec5beb8b0c
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2023-12-24 13:12:03 +0100

    refactor

--- example/main.c
+++ example/main.c
@@ -6,7 +6,7 @@
 /*   By: vlafouas <marvin@42.fr>                    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 17:11:45 by vlafouas          #+#    #+#             */
-/*   Updated: 2023/12/24 13:04:54 by poss             ###   ########.fr       */
+/*   Updated: 2023/12/24 13:11:36 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,14 +14,17 @@
 #include <fcntl.h>
 #include <stdio.h>
 
-#define PATH "./aux/41_no_nl"
-
-int	main(void)
+int	main(int argc, char** argv)
 {
 	int		fd;
 	char	*line;
 
-	fd = open(PATH, O_RDONLY);
+	if (argc != 2)
+	{
+		printf("Usage: %s text_file\n", argv[0]);
+		return 1;
+	}
+	fd = open(argv[1], O_RDONLY);
 	line = get_next_line(fd);
 	while (line)
 	{
