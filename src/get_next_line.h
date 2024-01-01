b24136bafb72870262fdf4fcedf0781e71eab51d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-01-01 13:46:33 +0100

    doc

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 12:32:34 by poss              #+#    #+#             */
-/*   Updated: 2024/01/01 13:43:18 by poss             ###   ########.fr       */
+/*   Updated: 2024/01/01 13:45:34 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -44,8 +44,11 @@ void					char_queue_push_back(t_char_queue **q, char c);
 char					char_queue_pop_front(t_char_queue **q);
 bool					char_queue_contains(const t_char_queue *q, char c);
 
-/** \fn size_t line_length(const t_char_queue* pop)
- *  \brief Compute the length of the line in the queue/buffer
+/** @fn size_t line_length(const t_char_queue* pop)
+ *  @brief Compute the length of the line in the queue/buffer
+ *
+ *  The line is ended either by a newline or by the end of the queue. The
+ *  newline is included in the count
  */
 size_t					line_length(const t_char_queue *q);
 char					*move_line_from_queue(t_char_queue **q);
