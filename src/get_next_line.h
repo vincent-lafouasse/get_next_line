a1347d15ede02a3b0bdee1d830cae7b5ef282e13
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-01-01 13:43:43 +0100

    doc

--- src/get_next_line.h
+++ src/get_next_line.h
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/24 12:32:34 by poss              #+#    #+#             */
-/*   Updated: 2024/01/01 13:26:25 by poss             ###   ########.fr       */
+/*   Updated: 2024/01/01 13:43:18 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -16,16 +16,22 @@
 # include <stdbool.h>
 # include <sys/types.h>
 
+/** @fn char *get_next_line(int fd)
+ *  @brief Read and return a line from a fd.
+ *  @param fd The file descriptor to read from, the caller has ownership of it
+ *  @return a heap-allocated string containing the line read from the file, or
+ * 		`NULL` if there is nothing else to read or an error occurred
+ */
 char					*get_next_line(int fd);
 
-/** \struct t_char_queue
- *  \brief A Simple Queue of characters
+/** @struct t_char_queue
+ *  @brief A Simple Queue of characters
  *
  * it provides the following functions with the usual semantics:
- * Self new()
- * void push_back(char)
- * char pop_front()
- * bool contains(char)
+ * `Self new()`
+ * `void push_back(char)`
+ * `char pop_front()`
+ * `bool contains(char)`
  */
 struct s_char_queue;
 typedef struct s_char_queue
@@ -38,6 +44,9 @@ void					char_queue_push_back(t_char_queue **q, char c);
 char					char_queue_pop_front(t_char_queue **q);
 bool					char_queue_contains(const t_char_queue *q, char c);
 
+/** \fn size_t line_length(const t_char_queue* pop)
+ *  \brief Compute the length of the line in the queue/buffer
+ */
 size_t					line_length(const t_char_queue *q);
 char					*move_line_from_queue(t_char_queue **q);
 ssize_t					load_queue(t_char_queue **q, int fd,
