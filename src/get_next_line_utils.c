4de65b396675630b6711951143fd8c3092f2675d
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-05 18:17:51 +0200

    load parts of libft

--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 17:53:56 by poss              #+#    #+#             */
-/*   Updated: 2024/04/05 17:54:01 by poss             ###   ########.fr       */
+/*   Updated: 2024/04/05 18:16:55 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,71 +14,66 @@
 #include "get_next_line.h"
 #include <stdlib.h>
 
-t_char_queue	*char_queue_new(char c)
-{
-	t_char_queue	*new;
+typedef unsigned char t_byte;
 
-	new = malloc(sizeof(*new));
-	if (!new)
+void	*ft_memcpy(void *dest, const void *src, size_t n)
+{
+	t_byte	*dest_bytes;
+	t_byte	*src_bytes;
+
+	dest_bytes = (t_byte *)dest;
+	src_bytes = (t_byte *)src;
+	while (n)
+	{
+		*dest_bytes = *src_bytes;
+		n--;
+		dest_bytes++;
+		src_bytes++;
+	}
+	return (dest);
+}
+
+char	*ft_strjoin(char const *s1, char const *s2)
+{
+	size_t	len1;
+	size_t	len2;
+	char	*out;
+
+	if (!s1 || !s2)
 		return (NULL);
-	new->c = c;
-	new->next = NULL;
-	return (new);
-}
-
-bool	char_queue_push_back(t_char_queue **q_ptr, char c)
-{
-	t_char_queue	*new;
-	t_char_queue	*current;
-
-	if (!q_ptr)
-		return (false);
-	new = char_queue_new(c);
-	if (!new)
-		return (false);
-	if (*q_ptr == NULL)
-	{
-		*q_ptr = new;
-		return (true);
-	}
-	current = *q_ptr;
-	while (current->next)
-	{
-		current = current->next;
-	}
-	current->next = new;
-	return (true);
-}
-
-char	char_queue_pop_front(t_char_queue **q_ptr)
-{
-	t_char_queue	*temp;
-	char			out;
-
-	if (!q_ptr || !*q_ptr)
-		return (-1);
-	out = (*q_ptr)->c;
-	temp = (*q_ptr)->next;
-	free(*q_ptr);
-	*q_ptr = temp;
+	len1 = ft_strlen(s1);
+	len2 = ft_strlen(s2);
+	out = malloc(len1 + len2 + 1);
+	if (!out)
+		return (NULL);
+	ft_memcpy(out, s1, len1);
+	ft_memcpy(out + len1, s2, len2);
+	out[len1 + len2] = '\0';
 	return (out);
 }
 
-bool	char_queue_contains(const t_char_queue *q, char c)
+char	*ft_strdup(const char *s)
 {
-	while (q)
-	{
-		if (q->c == c)
-			return (true);
-		q = q->next;
-	}
-	return (false);
+	char	*out;
+	size_t	len;
+
+	len = ft_strlen(s);
+	out = malloc(1 + len);
+	if (!out)
+		return (NULL);
+	ft_memcpy(out, s, 1 + len);
+	return (out);
 }
 
-void	char_queue_clear(t_char_queue **q_ptr)
+size_t	ft_strlen(const char *s)
 {
-	if (!q_ptr)
-		return ;
-	while (*q_ptr)
-		char_queue_pop_front(q_ptr);
+	size_t	len;
+
+	len = 0;
+	while (*s)
+	{
+		s++;
+		len++;
+	}
+	return (len);
 }
