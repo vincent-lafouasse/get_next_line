e70923704707d325acda78fbf62a6f7dd40a1216
vincent-lafouasse <vincent-lafouasse@users.noreply.github.com>
2024-04-05 19:25:51 +0200

    format

--- src/get_next_line.c
+++ src/get_next_line.c
@@ -6,7 +6,7 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/12/21 16:41:06 by vlafouas          #+#    #+#             */
-/*   Updated: 2024/04/05 19:14:23 by poss             ###   ########.fr       */
+/*   Updated: 2024/04/05 19:25:41 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,92 +15,102 @@
 #include <unistd.h>
 
 #ifndef BUFFER_SIZE
-# define BUFFER_SIZE 512
+#define BUFFER_SIZE 512
 #endif
 
 /* using the output of `ulimit -aH`, I do not like this,
-	this is probably not portable*/
+        this is probably not portable*/
 #define FD_HARD_LIMIT 1048576
 
 #ifndef MAX_FD
-# define MAX_FD FD_HARD_LIMIT
+#define MAX_FD FD_HARD_LIMIT
 #endif
 
 /*
 char	*get_next_line(int fd)
 {
-	static t_char_queue	*queue[MAX_FD];
-	ssize_t				bytes_read;
-	char* line;
+        static t_char_queue	*queue[MAX_FD];
+        ssize_t				bytes_read;
+        char* line;
 
-	if (BUFFER_SIZE <= 0 || fd < 0 || fd >= MAX_FD)
-		return (NULL);
-	if (char_queue_contains(queue[fd], '\n'))
-		return (move_line_from_queue(&queue[fd]));
-	bytes_read = load_queue(&queue[fd], fd);
-	if (bytes_read < 0)
-	{
-		char_queue_clear(&queue[fd]);
-		return (NULL);
-	}
-	if (bytes_read == 0 && !queue[fd])
-		return (NULL);
-	while (bytes_read == BUFFER_SIZE && !char_queue_contains(queue[fd], '\n'))
-		bytes_read = load_queue(&queue[fd], fd);
-	if (bytes_read < 0)
-	{
-		char_queue_clear(&queue[fd]);
-		return (NULL);
-	}
-	return (move_line_from_queue(&queue[fd]));
+        if (BUFFER_SIZE <= 0 || fd < 0 || fd >= MAX_FD)
+                return (NULL);
+        if (char_queue_contains(queue[fd], '\n'))
+                return (move_line_from_queue(&queue[fd]));
+        bytes_read = load_queue(&queue[fd], fd);
+        if (bytes_read < 0)
+        {
+                char_queue_clear(&queue[fd]);
+                return (NULL);
+        }
+        if (bytes_read == 0 && !queue[fd])
+                return (NULL);
+        while (bytes_read == BUFFER_SIZE && !char_queue_contains(queue[fd],
+'\n')) bytes_read = load_queue(&queue[fd], fd); if (bytes_read < 0)
+        {
+                char_queue_clear(&queue[fd]);
+                return (NULL);
+        }
+        return (move_line_from_queue(&queue[fd]));
 }
 */
 
 #define ARBITRARY_POSITIVE_VALUE 1
 
-ssize_t move_until_newline(char** line_ref, char* remaining)
-{
-	char *newline_pointer;
+#define INITIAL_CAPACITY 128
 
-	*line_ref = ft_strdup(remaining);
-	newline_pointer = ft_strchr(*line_ref, '\n');
-	if (newline_pointer == NULL)
-	{
-		*remaining = '\0';
-		return 0;
-	}
-	else
-	{
-		ft_memcpy(remaining, newline_pointer, ft_strlen(newline_pointer) + 1);
-		newline_pointer[1] = '\0';
-		return 0;
-	}
+typedef struct {
+  char *str;
+  size_t capacity;
+} t_string;
+
+t_string init_string(void) {
+  return (t_string){.str = malloc(INITIAL_CAPACITY),
+                    .capacity = INITIAL_CAPACITY};
 }
 
-ssize_t load_buffer(char* line, char* remaining, int fd)
-{
-	char buffer[BUFFER_SIZE];
-	ssize_t bytes_read = read(0, 0, 0);
-	return bytes_read;
+ssize_t move_until_newline(char **line_ref, char *remaining) {
+  char *newline_pointer;
+
+  *line_ref = ft_strdup(remaining);
+  newline_pointer = ft_strchr(*line_ref, '\n');
+  if (newline_pointer == NULL) {
+    *remaining = '\0';
+    return 0;
+  } else {
+    ft_memcpy(remaining, newline_pointer, ft_strlen(newline_pointer) + 1);
+    newline_pointer[1] = '\0';
+    return 0;
+  }
 }
 
-char	*get_next_line(int fd)
-{
-	static char remaining[BUFFER_SIZE + 1];
-	char* line = NULL;
-	ssize_t bytes_read;
+ssize_t load_buffer(char *line, char **remaining_ref, int fd) {
+  char buffer[BUFFER_SIZE];
+  ssize_t bytes_read = read(fd, buffer, BUFFER_SIZE);
+  free(*remaining_ref);
+  *remaining_ref = ft_substr(buffer, 0, bytes_read);
+  return bytes_read;
+}
 
-	if (*remaining)
-		move_until_newline(&line, remaining);
+char *get_next_line(int fd) {
+  static char *remaining = NULL;
+  char *line = NULL;
+  ssize_t bytes_read;
 
-	bytes_read = ARBITRARY_POSITIVE_VALUE;
-	while (bytes_read && !ft_strchr(line, '\n') && *remaining == '\0')
-		bytes_read = load_buffer(line, remaining, fd);
+  if (!remaining) {
+    remaining = malloc(1 + BUFFER_SIZE);
+    *remaining = '\0';
+  }
+  if (*remaining)
+    move_until_newline(&line, remaining);
 
-	if (*line == '\0')
-	{
-		free(line);
-		line = NULL;
-	}	
-	return line;
+  bytes_read = ARBITRARY_POSITIVE_VALUE;
+  while (bytes_read && !ft_strchr(line, '\n') && *remaining == '\0')
+    bytes_read = load_buffer(line, &remaining, fd);
+
+  if (*line == '\0') {
+    free(line);
+    line = NULL;
+  }
+  return line;
 }
--- src/get_next_line_utils.c
+++ src/get_next_line_utils.c
@@ -6,113 +6,115 @@
 /*   By: poss <marvin@42.fr>                        +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/04/05 17:53:56 by poss              #+#    #+#             */
-/*   Updated: 2024/04/05 19:15:14 by poss             ###   ########.fr       */
+/*   Updated: 2024/04/05 19:25:43 by poss             ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-
 
 #include "get_next_line.h"
 #include <stdlib.h>
 
 typedef unsigned char t_byte;
 
-void	*ft_memcpy(void *dest, const void *src, size_t n)
-{
-	t_byte	*dest_bytes;
-	t_byte	*src_bytes;
+void *ft_memcpy(void *dest, const void *src, size_t n) {
+  t_byte *dest_bytes;
+  t_byte *src_bytes;
 
-	dest_bytes = (t_byte *)dest;
-	src_bytes = (t_byte *)src;
-	while (n)
-	{
-		*dest_bytes = *src_bytes;
-		n--;
-		dest_bytes++;
-		src_bytes++;
-	}
-	return (dest);
+  dest_bytes = (t_byte *)dest;
+  src_bytes = (t_byte *)src;
+  while (n) {
+    *dest_bytes = *src_bytes;
+    n--;
+    dest_bytes++;
+    src_bytes++;
+  }
+  return (dest);
 }
 
-char	*ft_strjoin(char const *s1, char const *s2)
-{
-	size_t	len1;
-	size_t	len2;
-	char	*out;
+char *ft_strjoin(char const *s1, char const *s2) {
+  size_t len1;
+  size_t len2;
+  char *out;
 
-	if (!s1 || !s2)
-		return (NULL);
-	len1 = ft_strlen(s1);
-	len2 = ft_strlen(s2);
-	out = malloc(len1 + len2 + 1);
-	if (!out)
-		return (NULL);
-	ft_memcpy(out, s1, len1);
-	ft_memcpy(out + len1, s2, len2);
-	out[len1 + len2] = '\0';
-	return (out);
+  if (!s1 || !s2)
+    return (NULL);
+  len1 = ft_strlen(s1);
+  len2 = ft_strlen(s2);
+  out = malloc(len1 + len2 + 1);
+  if (!out)
+    return (NULL);
+  ft_memcpy(out, s1, len1);
+  ft_memcpy(out + len1, s2, len2);
+  out[len1 + len2] = '\0';
+  return (out);
 }
 
-char	*ft_strdup(const char *s)
-{
-	char	*out;
-	size_t	len;
+char *ft_strdup(const char *s) {
+  char *out;
+  size_t len;
 
-	len = ft_strlen(s);
-	out = malloc(1 + len);
-	if (!out)
-		return (NULL);
-	ft_memcpy(out, s, 1 + len);
-	return (out);
+  len = ft_strlen(s);
+  out = malloc(1 + len);
+  if (!out)
+    return (NULL);
+  ft_memcpy(out, s, 1 + len);
+  return (out);
 }
 
-size_t	ft_strlen(const char *s)
-{
-	size_t	len;
+size_t ft_strlen(const char *s) {
+  size_t len;
 
-	len = 0;
-	while (*s)
-	{
-		s++;
-		len++;
-	}
-	return (len);
+  len = 0;
+  while (*s) {
+    s++;
+    len++;
+  }
+  return (len);
 }
 
-char	*ft_strchr(const char *s, int c)
-{
-	while (1)
-	{
-		if (*s == (unsigned char)c)
-		{
-			return ((char *)s);
-		}
-		if (*s == '\0')
-		{
-			return (NULL);
-		}
-		s++;
-	}
+char *ft_strchr(const char *s, int c) {
+  while (1) {
+    if (*s == (unsigned char)c) {
+      return ((char *)s);
+    }
+    if (*s == '\0') {
+      return (NULL);
+    }
+    s++;
+  }
 }
 
-static size_t	min(size_t a, size_t b);
+size_t ft_strlcpy(char *dst, const char *src, size_t buffer_size) {
+  size_t i;
 
-char	*ft_substr(char const *s, unsigned int start, size_t len)
-{
-	char	*out;
-
-	if (start >= ft_strlen(s))
-		return (ft_strdup(""));
-	len = min(len, ft_strlen(s + start));
-	out = malloc(1 + len);
-	if (!out)
-		return (NULL);
-	ft_strlcpy(out, s + start, 1 + len);
-	return (out);
+  if (buffer_size == 0) {
+    return (ft_strlen(src));
+  }
+  i = 0;
+  while (src[i] && i < buffer_size - 1) {
+    dst[i] = src[i];
+    i++;
+  }
+  dst[i] = '\0';
+  return (ft_strlen(src));
 }
 
-static size_t	min(size_t a, size_t b)
-{
-	if (a < b)
-		return (a);
-	return (b);
+static size_t min(size_t a, size_t b);
+
+char *ft_substr(char const *s, unsigned int start, size_t len) {
+  char *out;
+
+  if (start >= ft_strlen(s))
+    return (ft_strdup(""));
+  len = min(len, ft_strlen(s + start));
+  out = malloc(1 + len);
+  if (!out)
+    return (NULL);
+  ft_strlcpy(out, s + start, 1 + len);
+  return (out);
+}
+
+static size_t min(size_t a, size_t b) {
+  if (a < b)
+    return (a);
+  return (b);
 }
